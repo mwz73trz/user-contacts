fdb4f5c3cdc456734a0d4287ad6e40e899eb6f97
Godwin Mshiu <mshiu95@gmail.com>
2023-10-02 12:13:53 -0400

    Create the first 3 tables

--- java/database/data.sql
+++ java/database/data.sql
@@ -2,5 +2,25 @@ BEGIN TRANSACTION;
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('user1','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('user2','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('admin1','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('user3','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 
+INSERT INTO office_details(office_name, phone_number, open_time, close_time, address, city, state, zip, service_fee )
+	VALUES  ('Mediwizard', '1-740-361-4729', '08:00:00', '17:00:00', '234 Diagon Ally', 'Columbus', 'Ohio', 43298, 85.00),
+			('Chase Clinic', '1-908-862-0280', '07:00:00', '18:00:00', '0972 Chase Place', 'Dalas', 'Texas', 10824, 90.00);
+			
+INSERT INTO employee(user_id, first_name, last_name, office_id)
+	VALUES( 1, 'User', 'One', 1),
+		   (2, 'Abby', 'Admin', 1),
+		   (5, 'Paul', 'Cross', 2);
+		  
+INSERT INTO patient(first_name, last_name, user_id)
+	VALUES ('Chase', 'Test', 3),
+		   ('Ginny', 'Weasley', 4),
+		   ('Harry', 'Potter', 6);
+		  
+
+--ROLLBACK;
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,9 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS office_details CASCADE;
+DROP TABLE IF EXISTS employee CASCADE;
+DROP TABLE IF EXISTS patient CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -10,4 +13,46 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE office_details(
+	office_id SERIAL UNIQUE,
+	office_name VARCHAR(64) NOT NULL,
+	phone_number VARCHAR(64) NOT NULL,
+	open_time TIME,
+	close_time TIME,
+	address VARCHAR(64) NOT NULL,
+	city VARCHAR(64) NOT NULL,
+	state VARCHAR(64) NOT NULL,
+	zip VARCHAR(64) NOT NULL,
+	service_fee DECIMAL(10, 2),
+	
+	CONSTRAINT PK_office_details PRIMARY KEY(office_id)
+);
+
+CREATE TABLE employee (
+	employee_id SERIAL UNIQUE,
+	user_id INT NOT NULL,
+	first_name VARCHAR (64) NOT NULL,
+	last_name VARCHAR (64) NOT NULL,
+	office_id INT NOT NULL,
+	
+	CONSTRAINT PK_employee_id PRIMARY KEY (employee_id),
+	CONSTRAINT FK_employee_users FOREIGN KEY(user_id) REFERENCES users(user_id),
+	CONSTRAINT FK_employee_office_details FOREIGN KEY(office_id) REFERENCES office_details(office_id)
+);
+
+CREATE TABLE  patient (
+	patient_id SERIAL UNIQUE,
+	first_name VARCHAR(64) NOT NULL,
+	last_name VARCHAR(64) NOT NULL,
+	user_id INT NOT NULL,
+	
+	CONSTRAINT PK_patient PRIMARY KEY(patient_id),
+	CONSTRAINT FK_patient_users FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+
+
+
+--ROLLBACK;
 COMMIT TRANSACTION;
+
