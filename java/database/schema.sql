d2460234beb4cf5a7e3a011ac02b6a91af86014b
Deanna Gibson <d.gibson1511@gmail.com>
2023-10-03 10:18:54 -0400

    Updated databash to incluede review tables, schedule and appointment tables

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,5 +1,10 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS review_reply CASCADE;
+DROP TABLE IF EXISTS review_details CASCADE;
+DROP TABLE IF EXISTS review CASCADE;
+DROP TABLE IF EXISTS appointment CASCADE;
+DROP TABLE IF EXISTS employee_schedule CASCADE;
 DROP TABLE IF EXISTS office_details CASCADE;
 DROP TABLE IF EXISTS employee CASCADE;
 DROP TABLE IF EXISTS patient CASCADE;
@@ -13,6 +18,24 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE  patient (
+	patient_id SERIAL UNIQUE,
+	first_name VARCHAR(64) NOT NULL,
+	last_name VARCHAR(64) NOT NULL,
+	user_id INT NOT NULL,
+	
+	CONSTRAINT PK_patient PRIMARY KEY(patient_id),
+	CONSTRAINT FK_patient_users FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE review(
+	review_id SERIAL, 
+	patient_id INT NOT NULL,
+	
+	CONSTRAINT PK_review PRIMARY KEY (review_id),
+	CONSTRAINT FK_review_patient FOREIGN KEY (patient_id) REFERENCES patient (patient_id)
+);
+
 CREATE TABLE office_details(
 	office_id SERIAL UNIQUE,
 	office_name VARCHAR(64) NOT NULL,
@@ -24,8 +47,10 @@ CREATE TABLE office_details(
 	state VARCHAR(64) NOT NULL,
 	zip VARCHAR(64) NOT NULL,
 	service_fee DECIMAL(10, 2),
+	review_id INT,
 	
-	CONSTRAINT PK_office_details PRIMARY KEY(office_id)
+	CONSTRAINT PK_office_details PRIMARY KEY(office_id),
+	CONSTRAINT FK_office_details_review FOREIGN KEY(review_id) REFERENCES review(review_id)
 );
 
 CREATE TABLE employee (
@@ -40,18 +65,57 @@ CREATE TABLE employee (
 	CONSTRAINT FK_employee_office_details FOREIGN KEY(office_id) REFERENCES office_details(office_id)
 );
 
-CREATE TABLE  patient (
-	patient_id SERIAL UNIQUE,
-	first_name VARCHAR(64) NOT NULL,
-	last_name VARCHAR(64) NOT NULL,
-	user_id INT NOT NULL,
+CREATE TABLE review_details(
+	review_id INT NOT NULL,
+	employee_id INT,
+	patient_id INT,
+	office_id INT NOT NULL, 
+	review TEXT NOT NULL,
 	
-	CONSTRAINT PK_patient PRIMARY KEY(patient_id),
-	CONSTRAINT FK_patient_users FOREIGN KEY(user_id) REFERENCES users(user_id)
+	CONSTRAINT FK_review_details_review FOREIGN KEY (review_id) REFERENCES review(review_id),
+	CONSTRAINT FK_review_details_employee FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
+	CONSTRAINT FK_review_details_patient FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
+	CONSTRAINT FK_review_details_office_details FOREIGN KEY(office_id) REFERENCES office_details(office_id)
+);
+
+CREATE TABLE review_reply(
+	review_id INT,
+	employee_id INT,
+	patient_id INT,
+	office_id INT NOT NULL, 
+	review_reply TEXT NOT NULL,
+	
+	CONSTRAINT FK_review_reply_review FOREIGN KEY (review_id) REFERENCES review(review_id),
+	CONSTRAINT FK_review_reply_employee FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
+	CONSTRAINT FK_review_reply_patient FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
+	CONSTRAINT FK_review_reply_office_details FOREIGN KEY(office_id) REFERENCES office_details(office_id)
 );
 
 
+CREATE TABLE employee_schedule(
+	schedule_id SERIAL,
+	employee_id INT NOT NULL,
+	start_time TIME NOT NULL,
+	end_time TIME NOT NULL,
+	isBooked BOOLEAN,
+	
+	CONSTRAINT PK_employee_schedule PRIMARY KEY (schedule_id),
+	CONSTRAINT FK_employee_schedule_employee FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
+);
 
+CREATE TABLE appointment(
+	appointment_id SERIAL,
+	created_time TIMESTAMP NOT NULL,
+	patient_id INT NOT NULL,
+	employee_id INT NOT NULL,
+	appointment_date DATE NOT NULL,
+	start_time TIME NOT NULL,
+	end_time TIME NOT NULL,
+		
+	CONSTRAINT PK_appointment PRIMARY KEY (appointment_id),
+	CONSTRAINT FK_appointment_employee FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
+	CONSTRAINT FK_appointment_patient FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
+);
 
 --ROLLBACK;
 COMMIT TRANSACTION;
