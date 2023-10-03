46e571c83d611bbf7b7f04a3ae5cd892f1fda951
Michael Zura <michaelzura@bellsouth.net>
2023-10-03 10:17:42 -0400

    When patient/employee logs in goes to component dependent on role

---
+++ vue/src/components/PatientLanding.vue
@@ -0,0 +1,13 @@
+<template>
+  <div><h1>Patient</h1></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,17 +2,25 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+    <p>{{ this.$store.state.user.authorities[0].name }}</p>
     <test-list> </test-list>
+    <patient-landing v-if="this.$store.state.user.authorities[0].name === 'ROLE_USER'" />
+    <employee-landing v-else-if="this.$store.state.user.authorities[0].name === 'ROLE_ADMIN'" />
   </div>
 </template>
 
 <script>
+import EmployeeLanding from '../components/EmployeeLanding.vue';
+import PatientLanding from '../components/PatientLanding.vue';
 import TestList from "../components/TestList.vue"
+
 export default 
   {
   name: "home",
   components:{
-    TestList
+    TestList,
+    PatientLanding,
+    EmployeeLanding
   }
 };
 </script>
