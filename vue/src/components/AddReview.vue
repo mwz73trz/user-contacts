3cc63fd363c9acb3b0893eb4a70566ba01b4bfd0
Godwin Mshiu <mshiu95@gmail.com>
2023-10-05 14:00:35 -0400

    Created Review list and add new review

---
+++ vue/src/components/AddReview.vue
@@ -0,0 +1,34 @@
+<template>
+  <form v-on:submit.prevent="addNewReview">
+    <div class="form-element">
+      <label for="review">Review:</label>
+      <textarea id="review" v-model="newReview.review"></textarea>
+      <button>Submit</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "add-review",
+  data() {
+    return {
+      newReview: {
+        id: 0,
+        userId: 0,
+        date: null,
+        review: "",
+        officeId: 0,
+      },
+    };
+  },
+  methods: {
+    addNewReview() {
+        
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue/src/components/ReviewList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="reviews">
+    <div
+      class="review"
+      v-for="review in this.$store.state.reviews"
+      v-bind:key="review.id"
+    >
+      <h2>Reviews</h2>
+      <p class="review-date">{{review.date}}</p>
+      <p>Hi{{review.review}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import ReviewServices from "../services/ReviewServices";
+export default {
+  name: "review-list",
+  data() {
+    return {
+      reviewList: [],
+      review: {
+        id: 0,
+        userId: 0,
+        date: null,
+        review: "",
+        officeId: 0,
+      },
+    };
+  },
+  created() {
+    ReviewServices.getReviews().then(response => {
+        this.reviewList = response.data;
+    });
+  },
+};
+</script>
+
+<style>
+</style>
