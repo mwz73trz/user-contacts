0430499cb4305378bdafb4350ba4b676a9e6f560
Deanna Gibson <d.gibson1511@gmail.com>
2023-10-05 16:51:02 -0400

    updated calendar component

--- vue/src/components/CalendarScheduler.vue
+++ vue/src/components/CalendarScheduler.vue
@@ -1,18 +1,72 @@
 <template>
-<div>
-  <div>
-      <input type="date" name="date" v-model="date" v-on:change="selectedDate($event)">
-      <input type="time">
-  </div>
-  </div>
+    <DayPilotCalendar id="dp" :config="config" ref="scheduler" />
 </template>
 
 <script>
-
+import {DayPilot, DayPilotCalendar} from '@daypilot/daypilot-lite-vue'
+// import {onMounted, ref} from "vue";
+import employeeServices from '../services/EmployeeServices'
 
 export default {
-    
+  name: 'Scheduler',
+  props: {
+  },
+  data: function() {
+    return {
+      employee:{
+        id: 0,
+        startTime: "",
+        endTime: "",
+        },
+      schedule: [],
+
+      //how will the calendar display
+      config: {
+      viewType: "Week",
+      startDate: DayPilot.Date.today(),
+      },
+    }
+  },
+  components: {
+    DayPilotCalendar
+  },
+
+ created() {
+    employeeServices.getEmployeeById(this.$route.params.employeeId).then(response => {
+      this.employee = response.data;
+    }),
+    employeeServices.getScheduleByEmployeeId(this.$store.state.user.id).then((response) => {
+      this.schedule = response.data;
+    });
+  },
+
+  computed: {
+    calendar() {
+      return this.$refs.scheduler.control;
+  },
+    calendarEvents(){
+      return this.schedule.map((event) => ({
+        id: event.schedule.id,
+        start: event.startTime,
+        end: event.endTime,
+        text: "Available"
+      }))
+    }
+  },
+
+  methods: {
+    loadAppointmentEvents(){
+    this.calendar.update({events: this.calendarEvents}); //returns the "events" and redraws the UI
+    }
+  },
+
+  mounted() {
+    this.loadAppointmentEvents();
+  },
+  
 }
+
+
 </script>
 
 <style>
